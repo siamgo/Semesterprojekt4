93aa190ef4eb90233166224ef9ff62b38f063fcf
JakobVork <201404709@post.au.dk>
2016-02-11 17:04:34 +0100

    Initial commit

---
+++ Plooder/Plooder.Unit.Test/PlooderLib.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Plooder.Unit.Test
+{
+    [TestFixture]
+    public class PlooderTest
+    {
+        [Test]
+        public void Add_2and2_Equals4()
+        {
+            PlooderLibClass plooder = new PlooderLibClass();
+            Assert.That(plooder.Add(2,2), Is.EqualTo(4));
+        }
+    }
+}
