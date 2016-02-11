4d969a57a33a0ec05ffec96c15a516714fcf99c6
JakobVork <201404709@post.au.dk>
2016-02-11 19:44:07 +0100

    Subtract added + tests

--- Plooder/Plooder.Unit.Test/PlooderLib.Test.cs
+++ Plooder/Plooder.Unit.Test/PlooderLib.Test.cs
@@ -10,11 +10,24 @@ namespace Plooder.Unit.Test
     [TestFixture]
     public class PlooderTest
     {
+        private PlooderLibClass _plooder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _plooder = new PlooderLibClass();
+        }
+
         [Test]
         public void Add_2and2_Equals4()
         {
-            PlooderLibClass plooder = new PlooderLibClass();
-            Assert.That(plooder.Add(2,2), Is.EqualTo(4));
+            Assert.That(_plooder.Add(2,2), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Subtract_4from3_Equals1()
+        {
+            Assert.That(_plooder.Subtract(4, 3), Is.EqualTo(1));
         }
     }
 }
